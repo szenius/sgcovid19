60970d3254e272bb324ee1bd795424e013364659
Sze Ying <ting.szeying@gmail.com>
2020-04-30 22:30:19 +0800

    feat: added page totle and meta stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import '../css/global.css';
 
 export const Footer = () => {
-    return (
-        <div className="footer">
-            By <a href="https://dev.to/szenius">Sze Ying</a> | <a href="https://github.com/szenius/covid19sg-viz">Source code</a>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      By{' '}
+      <a href="https://dev.to/szenius" target="_blank" rel="noopener noreferrer">
+        Sze Ying
+      </a>{' '}
+      |{' '}
+      <a href="https://github.com/szenius/covid19sg-viz" target="_blank" rel="noopener noreferrer">
+        Source code
+      </a>
+    </div>
+  );
+};
